522ee33c144e16c3cb8e214166cac3d0b9c9e9f4
Deepanjan008 <deepanjanbiswas13@gmail.com>
2026-06-28 12:56:32 +0530

    Refactor: Update root package name to com.nexuralabs.calculator (Fixes #300)

--- app/src/main/java/com/deepanjan/neocalculator/data/AppDatabase.kt
+++
@@ -1,9 +0,0 @@
-package com.deepanjan.neocalculator.data
-
-import androidx.room.Database
-import androidx.room.RoomDatabase
-
-@Database(entities = [HistoryEntity::class], version = 1)
-abstract class AppDatabase : RoomDatabase() {
-    abstract fun historyDao(): HistoryDao
-}
